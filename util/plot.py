09beffb09de32ddb74b7e47203c71ed5f6781082
eckhardt <benjamin.eckhardt@ut.ee>
2023-10-15 17:07:35 +0300

    main: meta, run, plotting

--- util/plot.py
+++ util/plot.py
@@ -1,6 +1,9 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import colorcet
+import pandas as pd
+import skimage
 
 
 def plot_mask(mask, image=None, color='red', ax=None, **figargs):
@@ -18,3 +21,33 @@ def plot_mask(mask, image=None, color='red', ax=None, **figargs):
   ax.contourf(mask, cmap=mk_cmap())
 
   return ax
+
+
+def plot_instseg(image=None, results=pd.DataFrame(), dims=['Var 1', 'Var 2'], what='Undefined'):
+  colors = colorcet.m_glasbey.colors
+
+  fig, axs = plt.subplots(len(results.columns), len(results.index), figsize=(10, 10))
+  if len(results.index)   <= 1: axs = [axs]
+  if len(results.columns) <= 1: axs = [[ax] for ax in axs]
+
+  plt.tight_layout()
+
+  for row, Y in zip(axs, results.index):
+    for ax, X in zip(row, results.columns):
+      ax.set_title(f"{what}: {dims[0]}={X}, {dims[1]}={Y}")
+      ax.axis('off')
+
+      insts = results[X][Y]
+
+      mask = np.zeros(image.shape[:2])
+      for i, inst in enumerate(insts):
+        mask[inst['segmentation']] = i+1
+
+      ax.imshow(skimage.color.label2rgb(
+        mask, image, saturation=1, bg_color=None, alpha=0.5, colors=colors)
+      )
+
+      for color, inst in zip(colors, insts):
+        ax.contour(inst['segmentation'], colors=[color], linewidths=2)
+
+  return fig, axs
